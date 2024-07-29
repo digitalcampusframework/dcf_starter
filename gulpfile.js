ad19ca0c1eb1642fcfbf739a2f0fd9f94e492af4
Thomas Neumann <thomasneumann21800@gmail.com>
2024-07-29 09:44:12 -0500

    Updated package.json, dependencies, and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,40 @@
-(() => {
+const { series, parallel, src, dest, watch } = require('gulp');
+const noop          = require('gulp-noop');
+const sass            = require('gulp-sass')(require('sass'));
+const sassGlob        = require('gulp-sass-glob');
+const postcss         = require('gulp-postcss');
+const autoprefixer = require('autoprefixer')
+const sourcemaps      = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
 
-  'use strict';
+const devBuild  = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development');
 
-  /**************** Gulp.js 4 configuration ****************/
-
-  const
-    // development or production
-    devBuild  = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development'),
-
-    // modules
-    gulp            = require('gulp'),
-    del             = require('del'),
-    noop            = require('gulp-noop'),
-    size            = require('gulp-size'),
-    sass            = require('gulp-sass'),
-    sassGlob        = require('gulp-sass-glob'),
-    autoprefixer    = require('autoprefixer'),
-    postcss         = require('gulp-postcss'),
-    sourcemaps      = require('gulp-sourcemaps'),
-    babel = require('gulp-babel'),
-    plumber = require('gulp-plumber'),
-    uglify = require('gulp-uglify');
-
-  console.log('Gulp', devBuild ? 'development' : 'production', 'build');
-
-
-  /**************** clean tasks ****************/
-
-  function cleanCSS() {
-    return del([ 'css' ]);
-
-  }
-  exports.cleanCSS = cleanCSS;
-
-  function cleanJS() {
-    return del([ 'js' ]);
-
-  }
-  exports.cleanJS = cleanJS;
-
-  /**************** CSS task ****************/
-
-  const cssConfig = {
-
+const cssConfig = {
     src         : 'scss/{,*/}*.{scss,sass}',
     watch       : 'scss/{,*/}*.{scss,sass}',
     build       : 'css/',
-    plugins:    [
-      autoprefixer()
+    plugins     : [ 
+        autoprefixer()
     ],
     sassOpts: {
-      sourceMap       : true,
-      outputStyle     : 'nested',
-      includePaths:   [ 'node_modules/modularscale-sass/stylesheets' ],
-      precision       : 2,
-      errLogToConsole : true
+        sourceMap       : true,
+        outputStyle     : 'compressed',
+        includePaths:   [ 'node_modules/modularscale-sass/stylesheets' ],
+        precision       : 2,
+        errLogToConsole : true
     }
+};
 
-  };
-
-  const jsConfig = {
+const jsConfig = {
     src:                'node_modules/dcf/js/',
     compileJs:          true
-  }
+}
 
-  // List of DCF Modules to process comment out or remove unwanted modules
-  // Values must match file names in `node_modules/dcf/js`
-  const jsModules = [
+// List of DCF Modules to process comment out or remove unwanted modules
+// Values must match file names in `node_modules/dcf/js`
+const jsModules = [
     'dcf-cardAsLink.js',
     'dcf-autoplayVideoToggle.js',
     'dcf-datepicker.js',
@@ -79,94 +47,78 @@
     'dcf-table.js',
     'dcf-tabs.js',
     'dcf-utility.js'  // Always include due to dependency with of some modules
-  ]
+];
 
-  function css() {
-    return gulp.src(cssConfig.src)
-      .pipe(sourcemaps ? sourcemaps.init() : noop())
-      .pipe(sassGlob())
-      .pipe(sass(cssConfig.sassOpts).on('error', sass.logError))
-      .pipe(postcss(cssConfig.plugins))
-      .pipe(sourcemaps ? sourcemaps.write('.') : noop())
-      .pipe(size({ showFiles:true }))
-      .pipe(gulp.dest(cssConfig.build));
-  }
-  exports.css = gulp.series(cleanCSS, css);
+function css() {
+    return src(cssConfig.src)
+        .pipe(sourcemaps ? sourcemaps.init() : noop())
+        .pipe(sassGlob())
+        .pipe(sass(cssConfig.sassOpts).on('error', sass.logError))
+        .pipe(postcss(cssConfig.plugins))
+        .pipe(sourcemaps ? sourcemaps.write('.') : noop())
+        .pipe(dest(cssConfig.build));
+}
+exports.css = css;
 
-  function vendorJS(cb) {
+function vendorJS(cb) {
     // copy body-scroll-lock
-    gulp.src('node_modules/body-scroll-lock/lib/bodyScrollLock.min.js')
-      .pipe(gulp.dest('js/vendor/'));
+    src('node_modules/body-scroll-lock/lib/bodyScrollLock.min.js')
+        .pipe(dest('js/vendor/'));
 
     // copy requirejs
-    gulp.src('node_modules/requirejs/require.js')
-      .pipe(gulp.dest('js/vendor/'));
+    src('node_modules/requirejs/require.js')
+        .pipe(dest('js/vendor/'));
 
     cb();
-  }
-  exports.vendorJS = vendorJS;
+}
+exports.vendorJS = vendorJS;
 
-  function localJS(done) {
-    return gulp.src('./js-src/*js')
-      .pipe(plumber())
-      // Transpile the JS code using Babel's preset-env.
-      .pipe(babel({
-        presets: [
-          ['@babel/env', {
-            modules: false
-          }]
-        ]
-      }))
-      .pipe(uglify())
-      .pipe(gulp.dest('js/'));
-  }
-  exports.localJS = localJS;
-
-  function js(done) {
-
-    let jsFiles = [];
-    if (jsConfig.compileJs) {
-      // Note: Always include DCFUtility since some modules are depended on
-      jsModules.forEach((module) => {
-        console.log('Including module ' + module + '...');
-        jsFiles.push(jsConfig.src + module);
-      });
-    }
-
-    if (jsFiles.length) {
-      return gulp.src(jsFiles)
+function localJS(done) {
+    return src('./js-src/*js')
         .pipe(plumber())
         // Transpile the JS code using Babel's preset-env.
         .pipe(babel({
-          presets: [
+        presets: [
             ['@babel/env', {
-              modules: 'amd'
+            modules: false
             }]
-          ],
-          plugins: [
-            '@babel/plugin-proposal-class-properties'
-          ]
+        ]
         }))
         .pipe(uglify())
-        .pipe(gulp.dest('js/'));
-    } else {
-      done();
+        .pipe(dest('js/'));
+}
+exports.localJS = localJS;
+
+function js(done) {
+
+    let jsFiles = [];
+    if (jsConfig.compileJs) {
+        // Note: Always include DCFUtility since some modules are depended on
+        jsModules.forEach((module) => {
+            console.log('Including module ' + module + '...');
+            jsFiles.push(jsConfig.src + module);
+        });
     }
-  }
-  exports.js = js;
 
-  /**************** watch task ****************/
+    if (jsFiles.length) {
+        return src(jsFiles)
+        .pipe(plumber())
+        // Transpile the JS code using Babel's preset-env.
+        .pipe(babel({
+            presets: [
+            ['@babel/env', {
+                modules: 'amd'
+            }]
+            ]
+        }))
+        .pipe(uglify())
+        .pipe(dest('js/'));
+    } else {
+        done();
+    }
+}
+exports.js = js;
 
-  function watch(done) {
-    // CSS changes
-    gulp.watch(cssConfig.watch, css);
-
-    done();
-  }
-
-  /**************** default task ****************/
-  exports.scripts =  gulp.series(exports.cleanJS, exports.js, exports.vendorJS, exports.localJS);
-  exports.styles = gulp.series(exports.css, watch);
-  exports.default = gulp.series(gulp.parallel(exports.scripts, exports.styles));
-
-})();
+exports.scripts =  series(exports.js, exports.vendorJS, exports.localJS);
+exports.styles = series(exports.css);
+exports.default = series(parallel(exports.scripts, exports.styles));
