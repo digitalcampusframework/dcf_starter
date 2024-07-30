f452fce388a43885deaa3743d0fd240117eefe8f
Ryan Dee <ryan@skoolbus39.com>
2024-07-30 10:31:53 -0500

    Add figcaption toggle to starter-main.js

--- js-src/starter-main.js
+++ js-src/starter-main.js
@@ -82,6 +82,35 @@ require(['js/dcf-autoplayVideoToggle'], function(DCFAutoplayVideoToggleModule) {
   autoplayVideoToggle.initialize();
 });
 
+// Figcaption Toggle
+require(['js/dcf-figcaption-toggle'], function(DCFFigcaptionToggleModule) {
+  // Get all the buttons and create the theme
+  const figcaptions = document.querySelectorAll('.dcf-figcaption-toggle');
+  const figcaptionToggleTheme = new DCFFigcaptionToggleModule.DCFFigcaptionToggleTheme();
+
+  figcaptionToggleTheme.setThemeVariable('toggleButtonInnerHTML',
+    `<svg class="dcf-h-4 dcf-w-4 dcf-fill-current"
+      width="24" height="24" viewBox="0 0 24 24" focusable="false" aria-hidden="true">
+      <path class="theme-btn-toggle-figcaption-icon-open"
+      d="M1,3h19c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1C0.4,1,0,1.4,0,2C0,2.6,0.4,3,1,3z"/>
+      '<path class="theme-btn-toggle-figcaption-icon-open"
+      d="M1,8h15c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1C0.4,6,0,6.4,0,7C0,7.6,0.4,8,1,8z"/>
+      <path class="theme-btn-toggle-figcaption-icon-close-1"
+      d="M1,13h22c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,12.6,0.4,13,1,13z"/>
+      <path class="theme-btn-toggle-figcaption-icon-close-2"
+      d="M1,13h22c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,12.6,0.4,13,1,13z"/>
+      <path class="theme-btn-toggle-figcaption-icon-open"
+      d="M1,18h18c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,17.6,0.4,18,1,18z"/>
+      <path class="theme-btn-toggle-figcaption-icon-open"
+      d="M1,23h15c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,22.6,0.4,23,1,23z"/>
+    </svg>`
+  );
+
+  // Initialize the buttons with the theme
+  const figcaptionToggleObj = new DCFFigcaptionToggleModule.DCFFigcaptionToggle(figcaptions, figcaptionToggleTheme);
+  figcaptionToggleObj.initialize();
+});
+
 // Slideshow Example
 require(['js/dcf-slideshow'], function(DCFSlideshowModule) {
   const slideshows = document.querySelectorAll('.dcf-slideshow');
